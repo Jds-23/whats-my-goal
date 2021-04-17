b4f9a71b21f6cc9c32cba854cdbfa72e4f48f418
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-04-17 17:31:45 +0530

    feat: add router to the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import { Task } from "./components/Task/";
-import { v4 as uuidv4 } from "uuid";
-import { TaskListHeader } from "./components/TaskListHeader/";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-interface TasksInterface {
-  completed: boolean;
-  title: string;
-  id: string;
-}
-function App() {
-  const [title, setTitle] = useState("");
-  const [nofActiveTask, setNofActiveTask] = useState(0);
-  const [tasks, setTasks] = useState<TasksInterface[]>([]);
-  const addTask = () => {
-    if (title !== "") {
-      setTasks([...tasks, { title, completed: false, id: uuidv4() }]);
-      setNofActiveTask((prv) => prv + 1);
-      setTitle("");
-    }
-  };
-  const handleDone = (id: string) => {
-    let mapped = tasks.map((task) => {
-      if (task.id === id) {
-        if (!task.completed) {
-          setNofActiveTask((prv) => prv - 1);
-        } else {
-          setNofActiveTask((prv) => prv + 1);
-        }
-        return { ...task, completed: !task.completed };
-      } else {
-        return { ...task };
-      }
-    });
-    setTasks(mapped);
-  };
-  const handleDelete = (id: string) => {
-    let filtered = tasks.filter((task) => {
-      if (!task.completed && task.id === id) {
-        setNofActiveTask((prv) => prv - 1);
-      }
-      return task.id !== id;
-    });
-    setTasks(filtered);
-  };
-  const handleOnDragEnd = (result: any) => {
-    if (!result.destination) return;
-
-    const items = Array.from(tasks);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-
-    setTasks(items);
-  };
+import { Todo } from "./components/Pages/Todo";
+import { CreateTask } from "./components/Pages/CreateTask";
+const App = () => {
   return (
-    <div className="app">
-      <div>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <button onClick={addTask}>Add</button>
-      </div>
-      <TaskListHeader nofActiveTask={nofActiveTask} />
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="tasks">
-          {(provided) => (
-            <div {...provided.droppableProps} ref={provided.innerRef}>
-              {tasks.map(({ title, completed, id }, index) => {
-                return (
-                  <Draggable key={id} draggableId={id} index={index}>
-                    {(provided) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                      >
-                        <Task
-                          title={title}
-                          completed={completed}
-                          key={id}
-                          id={id}
-                          handleDone={handleDone}
-                          handleDelete={handleDelete}
-                        />
-                      </div>
-                    )}
-                  </Draggable>
-                );
-              })}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/create-task" component={CreateTask} />
+          <Route exact path="/" component={Todo} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Pages/Todo/Todo.tsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import "./Todo.css";
+import { Task } from "../../Task/";
+import { v4 as uuidv4 } from "uuid";
+import { TaskListHeader } from "../../TaskListHeader/";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+interface TasksInterface {
+  completed: boolean;
+  title: string;
+  id: string;
+}
+function Todo() {
+  const [title, setTitle] = useState("");
+  const [nofActiveTask, setNofActiveTask] = useState(0);
+  const [tasks, setTasks] = useState<TasksInterface[]>([]);
+  const addTask = () => {
+    if (title !== "") {
+      setTasks([...tasks, { title, completed: false, id: uuidv4() }]);
+      setNofActiveTask((prv) => prv + 1);
+      setTitle("");
+    }
+  };
+  const handleDone = (id: string) => {
+    let mapped = tasks.map((task) => {
+      if (task.id === id) {
+        if (!task.completed) {
+          setNofActiveTask((prv) => prv - 1);
+        } else {
+          setNofActiveTask((prv) => prv + 1);
+        }
+        return { ...task, completed: !task.completed };
+      } else {
+        return { ...task };
+      }
+    });
+    setTasks(mapped);
+  };
+  const handleDelete = (id: string) => {
+    let filtered = tasks.filter((task) => {
+      if (!task.completed && task.id === id) {
+        setNofActiveTask((prv) => prv - 1);
+      }
+      return task.id !== id;
+    });
+    setTasks(filtered);
+  };
+  const handleOnDragEnd = (result: any) => {
+    if (!result.destination) return;
+
+    const items = Array.from(tasks);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setTasks(items);
+  };
+  return (
+    <div className="app">
+      <div>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <TaskListHeader nofActiveTask={nofActiveTask} />
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="tasks">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {tasks.map(({ title, completed, id }, index) => {
+                return (
+                  <Draggable key={id} draggableId={id} index={index}>
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Task
+                          title={title}
+                          completed={completed}
+                          key={id}
+                          id={id}
+                          handleDone={handleDone}
+                          handleDelete={handleDelete}
+                        />
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default Todo;
