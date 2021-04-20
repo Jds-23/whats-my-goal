8a9b4ce5cd5d955508844c8836adec0c1c920232
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2021-04-20 12:10:43 +0530

    refractor:useHooks to use StateProvider

--- src/components/utils/hooks.tsx
+++ src/components/utils/hooks.tsx
@@ -1,26 +1,21 @@
-import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-interface TasksInterface {
-  completed: boolean;
-  title: string;
-  id: string;
-}
+import { useStateValue } from "../../context/StateProvider";
+
 export const useHooks = () => {
-  const [nofActiveTask, setNofActiveTask] = useState(0);
-  const [tasks, setTasks] = useState<TasksInterface[]>([]);
+  const { tasks, setTasks, nofActiveTask, setNofActiveTask } = useStateValue()!;
   const addTask = (title: string) => {
     if (title !== "") {
       setTasks([...tasks, { title, completed: false, id: uuidv4() }]);
-      setNofActiveTask((prv) => prv + 1);
+      setNofActiveTask(nofActiveTask + 1);
     }
   };
   const handleDone = (id: string) => {
     let mapped = tasks.map((task) => {
       if (task.id === id) {
         if (!task.completed) {
-          setNofActiveTask((prv) => prv - 1);
+          setNofActiveTask(nofActiveTask - 1);
         } else {
-          setNofActiveTask((prv) => prv + 1);
+          setNofActiveTask(nofActiveTask + 1);
         }
         return { ...task, completed: !task.completed };
       } else {
@@ -32,7 +27,7 @@ export const useHooks = () => {
   const handleDelete = (id: string) => {
     let filtered = tasks.filter((task) => {
       if (!task.completed && task.id === id) {
-        setNofActiveTask((prv) => prv - 1);
+        setNofActiveTask(nofActiveTask - 1);
       }
       return task.id !== id;
     });
